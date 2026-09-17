eddae192b297149af30af5e8f1bca162b613bce1
JASCOJI123 <jascovines@gmail.com>
2026-09-17 22:19:58 +0500

    refactor: load one unified design stylesheet

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,9 +5,6 @@ import App from './App';
 import ThemeToggle from './components/ThemeToggle';
 import { I18nProvider } from './lib/i18n';
 import './styles.css';
-import './unified-theme.css';
-import './theme-toggle.css';
-import './lib/i18n.css';
 
 initTelegram();
 
